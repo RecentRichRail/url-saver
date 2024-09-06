6b3f91be60bce92a99a7ed153390ee91e4c5bf9c
RecentRichRail <Matthewspicer.18@gmail.com>
2024-09-06 21:06:14 +0000

    Removed some prior testing code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///link_data.db'
